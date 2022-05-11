db2e6ca51445d164d8d464a541776db0fa6575e7
Barinov Maksim <craftyplonkton@gmail.com>
2022-05-12 00:32:39 +0300

    User model

---
+++ api_yamdb/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+urlpatterns = [
+    path('v1/auth/signup/', ),
+    path('v1/auth/token/', ),
+]
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,4 +3,30 @@ from django.db import models
 
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'Пользователь'),
+        ('moderator', 'Модератор'),
+        ('admin', 'Администратор')
+    )
     bio = models.TextField('Биография', blank=True)
+    email = models.EmailField('email адрес', max_length=254, unique=True)
+    confirmation_code = models.TextField(
+        'Код подтверждения',
+        blank=True
+    )
+    role = models.CharField(
+        'Првава доступа',
+        max_length=30,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=~models.Q(username='me'),
+                name='username_not_me')
+        ]
+
+    def __str__(self):
+        return self.username
