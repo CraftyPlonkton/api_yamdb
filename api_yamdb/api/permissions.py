da04be975f050871bf5fcff94714ee2585f892a5
Barinov Maksim <craftyplonkton@gmail.com>
2022-05-15 19:51:59 +0300

    users done

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,7 +1,36 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAdminOrOwner(BasePermission):
+class IsAdminOrSuperuserOnly(BasePermission):
     def has_permission(self, request, view):
-        return (not request.user.is_anonymous and
-                request.user.role == 'admin' or request.user.is_superuser)
+        return request.user.role == 'admin' or request.user.is_superuser
+
+
+class IsAdminOrModeratorOrOwnerOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user.is_superuser or
+                request.user.role == 'admin' or
+                request.user.role == 'moderator' or
+                request.user.username == obj.author.username)
+
+
+class IsAdminOrModeratorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user.is_superuser or
+                request.user.role == 'admin' or
+                request.user.role == 'moderator')
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user.is_superuser or
+                request.user.role == 'admin')
